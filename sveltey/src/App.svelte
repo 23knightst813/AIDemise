e595bfda0d2105e7a60ca01f8fbf9a87c1b0ee22
Theo <23knightst813@collyers.ac.uk>
2024-12-26 00:00:33 +0000

    initialize Svelte project with basic structure and components

---
+++ sveltey/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import InputBox from './lib/InputBox.svelte'
+  import Counter from './lib/Counter.svelte'
+  import '@fontsource/bad-script';
+
+</script>
+
+<main>
+
+  <h1>AiDemise</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <div class="card">
+    <InputBox />
+  </div>
+
+</main>
+
+<style>
+  :global(h1) {
+    font-family: "bad script";
+  }
+</style>
---
+++ sveltey/src/lib/InputBox.svelte
@@ -0,0 +1,18 @@
+<div>
+    <form>
+    <input type="text"/>
+    </form> 
+    <br>    
+<button type="submit">Submit</button>
+</div>
+
+<style>
+    input {
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        font-size: 16px;
+        width: 100%;
+        box-sizing: border-box;
+    }
+</style>
